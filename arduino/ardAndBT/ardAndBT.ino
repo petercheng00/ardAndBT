673f4a4b3b50bfd48296cdef66219fdc27502236
Wei Wu <wuhuwei@gmail.com>
2012-11-16 14:02:31 -0800

    adding code for sending signals -- pulseIR timing may not be right, we need the oscilloscope

--- arduino/ardAndBT/ardAndBT.ino
+++ arduino/ardAndBT/ardAndBT.ino
@@ -1,54 +1,96 @@
-#include <SoftwareSerial.h>
+const int LED_PIN1 = 11;
+const int LED_PIN2 = 12;
+const int LED_PIN3 = 13;
 
-int bluetoothTx = 2;
-int bluetoothRx = 3;
+int IRledPin =  14;
 
-const int LED_PIN1 = 9;
-const int LED_PIN2 = 10;
-const int LED_PIN3 = 11;
-
-int currReading = 0;
+String currReading = "";
 boolean toggle1 = true;
 boolean toggle2 = true;
 boolean toggle3 = true;
 
-SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+HardwareSerial bluetooth = HardwareSerial();
 
 void setup() {
     Serial.begin(9600);
     
-    bluetooth.begin(115200);
+    bluetooth.begin(9600);
     bluetooth.print("$$$");
     delay(100);
     bluetooth.println("U,9600,N");
     bluetooth.begin(9600);
     
-    pinMode(LED_PIN1, OUTPUT);
-    pinMode(LED_PIN2, OUTPUT);
-    pinMode(LED_PIN3, OUTPUT);
+    pinMode(IRledPin, OUTPUT);
 }
 
 void loop() {
-    //if (Serial.available()) {
-    //    currReading = Serial.read();
     if (bluetooth.available())
     {
       Serial.println("bla");
-        currReading = bluetooth.read();
-        if (currReading == 1)
-        {
-          toggle1 = !toggle1;
+        currReading= bluetooth.readStringUntil('\n');
+        Serial.println(currReading);
+        unsigned int currentNum = 0;   
+        int i;
+        for(i = 0; i < currReading.length(); i++) {
+          
+          if(currReading.charAt(i) >= '0' && currReading.charAt(i) <= '9') {
+            currentNum = currentNum * 10 + (currReading.charAt(i)-'0');
+          } else if(currReading.charAt(i) == ',') {
+              break;
+          } 
         }
-        else if (currReading == 2)
-        {
-          toggle2 = !toggle2;
+
+        unsigned int signal[currentNum];
+        unsigned int valuesSeen = 0;
+        int j;
+        currentNum = 0;
+        Serial.println("foo");
+        Serial.println(i);
+        for(j = i+1; j < currReading.length(); j++) {
+          if(currReading.charAt(j) >= '0' && currReading.charAt(j) <= '9') {
+        
+            currentNum = currentNum * 10 + (currReading.charAt(j)-'0');
+            
+          } else if(currReading.charAt(j) == ',') {
+            signal[valuesSeen] = currentNum;
+            currentNum = 0;
+            valuesSeen++;
+          } 
         }
-        else if (currReading == 3)
-        {
-          toggle3 = !toggle3;
-        }
+         signal[valuesSeen] = currentNum;
+         sendSignal(signal);
     }
-    digitalWrite(LED_PIN1, toggle1);
-    digitalWrite(LED_PIN2, toggle2);
-    digitalWrite(LED_PIN3, toggle3);
+}
+
+void sendSignal(unsigned int signal[]){
+  Serial.println("sending signal!");
+  for(int i = 0; i < sizeof(signal); i++) {
+    if(i % 2 == 0) {
+      delayMicroseconds(signal[i]);
+    } else {
+      pulseIR(signal[i]);
+    }
+  }
+}
+
+// This procedure sends a 38KHz pulse to the IRledPin 
+// for a certain # of microseconds. We'll use this whenever we need to send codes
+void pulseIR(long microsecs) {
+ 
+  // we'll count down from the number of microseconds we are told to wait
+ 
+  cli();  // this turns off any background interrupts
+ 
+  while (microsecs > 0) {
+    // 38 kHz is about 13 microseconds high and 13 microseconds low
+   digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
+   delayMicroseconds(9);         // hang out for 10 microseconds
+   digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
+   delayMicroseconds(9);         // hang out for 10 microseconds
+ 
+   // so 26 microseconds altogether
+   microsecs -= 26;
+  }
+ 
+  sei();  // this turns them back on
 }
