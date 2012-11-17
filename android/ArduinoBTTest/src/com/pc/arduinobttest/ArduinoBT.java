40a5ff10485ab2aa6ec3a61b3df0bcd5a2f48abf
Peter Cheng <petercheng00@gmail.com>
2012-11-16 22:08:12 -0800

    committing updated timings for arduino signal code. Added interface for connecting to BT devices and appropriate handling for disconnects, etc.

--- android/ArduinoBTTest/src/com/pc/arduinobttest/ArduinoBT.java
+++ android/ArduinoBTTest/src/com/pc/arduinobttest/ArduinoBT.java
@@ -1,33 +1,53 @@
 package com.pc.arduinobttest;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ArduinoBT extends Activity {
 	private BluetoothAdapter mBTAdapter;
-	private BluetoothDevice mBTDevice = null;
-	private BluetoothSocket mBTSocket;
-	private OutputStream mOutputStream;
-	private final String serialUUID = "00001101-0000-1000-8000-00805f9b34fb";
+    
+	private String mConnectedDeviceName = null;
+    // Member object for the chat services
+    private BluetoothService mBTService = null;
+	
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+
+    // The Handler that gets information back from the BluetoothChatService
+    private final Handler mHandler = new BTHandler();
+	
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    private static final String powerCode = "35560,2380,560,1200,580,600,560,1200,580,600,560,1200,580,620,560,600,560,1220,560,620,560,600,560,620,560,620,25300,2380,560,1200,580,600,560,1220,560,600,580,1200,560,600,580,600,580,1200,560,600,580,600,580,600,560,620,25300,2400,540,1200,580,600,580,1180,580,620,560,1200,560,620,560,620,560,1200,560,620,560,620,560,600,580,600\n";
+
+	
 	
 	private TextView statusText;
 	private Button button1;
 	private Button button2;
 	private Button button3;
+	private Button connect_button;
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,74 +58,110 @@ public class ArduinoBT extends Activity {
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
      	button3 = (Button)findViewById(R.id.button3);
+     	connect_button = (Button)findViewById(R.id.connect_button);
+     	
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBTAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        
+        mBTService = new BluetoothService(this, mHandler);
+
      	
      	button1.setOnClickListener(new OnClickListener()
      	{
      		public void onClick(View v)
      		{
-     			sendByte(1);
+     			sendCode(powerCode);
      		}     		
      	});
      	button2.setOnClickListener(new OnClickListener()
      	{
      		public void onClick(View v)
      		{
-     			sendByte(2);
+     			sendCode(powerCode);
      		}     		
      	});
      	button3.setOnClickListener(new OnClickListener()
      	{
      		public void onClick(View v)
      		{
-     			sendByte(3);
+     			sendCode(powerCode);
      		}     		
      	});
+     	connect_button.setOnClickListener(new OnClickListener()
+     	{
+     		public void onClick(View v)
+     		{
+     			connectToBT();
+     		}
+     	});
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
 
-     	mBTAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!mBTAdapter.isEnabled())
-        {
-        	Intent requestEnableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        	startActivity(requestEnableBT);
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBTAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
         }
-        statusText.setText("Connecting...");
-        Set<BluetoothDevice> pairedDevices = mBTAdapter.getBondedDevices();
-        for (BluetoothDevice device : pairedDevices)
-        {
-        	if (device.getName().equalsIgnoreCase("linvor"))
-        	{
-        		mBTDevice = device;
-        		break;
-        	}
-        }
-        if (mBTDevice == null)
-        {
-        	statusText.setText("NOT CONNECTED");
-        	//sad face
-        }
-        else
-        {
-        	UUID uuid = UUID.fromString(serialUUID);
-        	try {
-				mBTSocket = mBTDevice.createRfcommSocketToServiceRecord(uuid);
-	        	mBTSocket.connect();
-	        	mOutputStream = mBTSocket.getOutputStream();
-	        	statusText.setText("Connected");
-			} catch (IOException e) {
-				statusText.setText("FAILED TO CONNECT");
-				e.printStackTrace();
-			}
+    }
+    
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mBTService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mBTService.getState() == BluetoothService.STATE_NONE) {
+              // Start the Bluetooth chat services
+              mBTService.start();
+            }
         }
     }
 
-    public void sendByte(int i)
+    
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop the Bluetooth chat services
+        if (mBTService != null) mBTService.stop();
+    }
+
+    
+    public String formatIRCode(String code)
     {
-    	try {
-			mOutputStream.write(i);
-			statusText.setText("wrote something");
-		} catch (IOException e) {
-			statusText.setText("writing failed");
-			e.printStackTrace();
-		}
+    	int numNums = 0;
+    	for (int j = 0; j < code.length(); ++j)
+    	{
+    		if (code.charAt(j) == ',')
+    		{
+    			++numNums;
+    		}
+    	}
+    	return "" + (numNums+1) + "," + code;
+    }
+    public void sendCode(String code)
+    {
+    	if (mBTService.getState() == BluetoothService.STATE_CONNECTED)
+    	{
+    		mBTService.write(formatIRCode(code).getBytes());
+    	}
+    	else
+    	{
+    		connectToBT();
+    	}
     }
     
     @Override
@@ -113,4 +169,72 @@ public class ArduinoBT extends Activity {
         getMenuInflater().inflate(R.menu.activity_arduino_bt, menu);
         return true;
     }
+        
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case REQUEST_CONNECT_DEVICE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data);
+            }
+            break;
+        case REQUEST_ENABLE_BT:
+            // When the request to enable Bluetooth returns
+            if (resultCode == Activity.RESULT_OK) {
+            } else {
+                // User did not enable Bluetooth or an error occurred
+                Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private void connectDevice(Intent data) {
+        // Get the device MAC address
+        String address = data.getExtras()
+            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        BluetoothDevice device = mBTAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        mBTService.connect(device);
+    }
+    
+    private void connectToBT()
+	{
+        // Launch the DeviceListActivity to see devices and do scan
+        Intent listDevicesIntent = new Intent(this, DeviceListActivity.class);
+        startActivityForResult(listDevicesIntent, REQUEST_CONNECT_DEVICE);
+	}
+    
+    private class BTHandler extends Handler
+    {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MESSAGE_STATE_CHANGE:
+                switch (msg.arg1) {
+                case BluetoothService.STATE_CONNECTED:
+                	statusText.setText("Connected");
+                    break;
+                case BluetoothService.STATE_CONNECTING:
+                	statusText.setText("Connecting...");
+                    break;
+                case BluetoothService.STATE_NONE:
+                	statusText.setText("Not Connected");
+                    break;
+                }
+                break;
+            case MESSAGE_DEVICE_NAME:
+                // save the connected device's name
+                mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                Toast.makeText(getApplicationContext(), "Connected to "
+                               + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                break;
+            case MESSAGE_TOAST:
+                Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                               Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    };  
 }
--- arduino/ardAndBT/ardAndBT.ino
+++ arduino/ardAndBT/ardAndBT.ino
@@ -9,6 +9,8 @@ boolean toggle1 = true;
 boolean toggle2 = true;
 boolean toggle3 = true;
 
+boolean debugLED = false;
+
 HardwareSerial bluetooth = HardwareSerial();
 
 void setup() {
@@ -19,14 +21,18 @@ void setup() {
     delay(100);
     bluetooth.println("U,9600,N");
     bluetooth.begin(9600);
-    
+    pinMode(LED_PIN1, OUTPUT);
+    pinMode(LED_PIN2, OUTPUT);
+    pinMode(LED_PIN3, OUTPUT);
     pinMode(IRledPin, OUTPUT);
+    digitalWrite(LED_PIN1, HIGH);
+    digitalWrite(LED_PIN2, HIGH);
+    digitalWrite(LED_PIN3, HIGH);
 }
 
 void loop() {
     if (bluetooth.available())
     {
-      Serial.println("bla");
         currReading= bluetooth.readStringUntil('\n');
         Serial.println(currReading);
         unsigned int currentNum = 0;   
@@ -39,13 +45,10 @@ void loop() {
               break;
           } 
         }
-
         unsigned int signal[currentNum];
         unsigned int valuesSeen = 0;
         int j;
         currentNum = 0;
-        Serial.println("foo");
-        Serial.println(i);
         for(j = i+1; j < currReading.length(); j++) {
           if(currReading.charAt(j) >= '0' && currReading.charAt(j) <= '9') {
         
@@ -58,19 +61,27 @@ void loop() {
           } 
         }
          signal[valuesSeen] = currentNum;
-         sendSignal(signal);
+         sendSignal(signal, sizeof(signal)/sizeof(unsigned int));
     }
 }
 
-void sendSignal(unsigned int signal[]){
-  Serial.println("sending signal!");
-  for(int i = 0; i < sizeof(signal); i++) {
+void sendSignal(unsigned int signal[], int size){
+  //while(true){
+  for(int i = 0; i < size; i++) {
     if(i % 2 == 0) {
-      delayMicroseconds(signal[i]);
+      //Serial.println("waiting for");]
+      if(signal[i] > 16000) {
+        delay(signal[i]/1000);
+      } else {
+        delayMicroseconds(signal[i]+20);
+      }
     } else {
+      //Serial.println("pulsing for");
       pulseIR(signal[i]);
     }
+    //Serial.println(signal[i]);
   }
+  //}
 }
 
 // This procedure sends a 38KHz pulse to the IRledPin 
@@ -84,9 +95,9 @@ void pulseIR(long microsecs) {
   while (microsecs > 0) {
     // 38 kHz is about 13 microseconds high and 13 microseconds low
    digitalWrite(IRledPin, HIGH);  // this takes about 3 microseconds to happen
-   delayMicroseconds(9);         // hang out for 10 microseconds
+   delayMicroseconds(14);         // hang out for 10 microseconds
    digitalWrite(IRledPin, LOW);   // this also takes about 3 microseconds
-   delayMicroseconds(9);         // hang out for 10 microseconds
+   delayMicroseconds(15);         // hang out for 10 microseconds
  
    // so 26 microseconds altogether
    microsecs -= 26;
